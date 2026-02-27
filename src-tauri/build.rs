1bb69e87dd08358169b12d868bf72c48a4d23e0d
欧阳天 <2205593667@qq.com>
2026-02-27 20:39:31 +0800

    wip: 🚧 test 官网案例

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,21 +1,21 @@
 use std::env;
 use std::fs;
-use std::path::{ PathBuf};
+use std::path::PathBuf;
 fn main() {
     println!("cargo:rustc-link-search=native=libs");
     copy_dlls();
     tauri_build::build()
 }
-fn copy_dlls(){
+fn copy_dlls() {
     // 获取当前项目根目录 (src-tauri)
     let project_dir = env::current_dir().expect("Failed to get project dir");
     let libs_dir = project_dir.join("libs");
-    
+
     // 获取输出目录 (例如 target/debug)
     // 注意：OUT_DIR 是 build.rs 专用的临时目录，我们需要向上找三级到 target/debug
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
     let target_dir = PathBuf::from(out_dir)
-        .join("../../../") 
+        .join("../../../")
         .canonicalize()
         .expect("Failed to get target dir");
 
@@ -25,10 +25,10 @@ fn copy_dlls(){
             let path = entry.path();
             if path.extension().and_then(|s| s.to_str()) == Some("dll") {
                 let dest = target_dir.join(path.file_name().unwrap());
-                
+
                 // 执行拷贝：如果文件已存在且内容没变，fs::copy 会处理
                 fs::copy(&path, &dest).expect("Failed to copy DLL to target directory");
-                
+
                 // 打印信息（在执行 cargo build -v 时可见）
                 println!("cargo:warning=Auto-copied {} to {:?}", path.display(), dest);
             }
--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -1,25 +1,85 @@
 <script setup lang="ts">
 // import { listen } from '@tauri-apps/api/event'
-import { onResult, startListening, stopListening } from 'tauri-plugin-stt-api'
-
+import { onResult, onStateChange, requestPermission, startListening, stopListening } from 'tauri-plugin-stt-api'
+import { speak } from 'tauri-plugin-tts-api'
+// 定义 window 上的扩展类型
+interface Window {
+  sttListeners?: Array<(() => void) | { unregister: () => void }>
+}
+let unlisten: any
 async function startVoice() {
-  console.log(1111)
   // 开始录音
+  const perm = await requestPermission()
+  if (perm.microphone !== 'granted') {
+    console.error('Microphone permission required')
+    return
+  }
   try {
-    await stopListening()
+    unlisten && unlisten()
+    await stopListeningAll()
     await startListening({ language: 'zh-CN' })
     console.log('开始录音成功')
   }
   catch (error) {
     console.error('开始录音失败:', error)
   }
-
-  onResult((result) => {
-    console.log('Recognized:', result.transcript, result.isFinal)
+  console.log('注册了一次监听')
+  await onStateChange((event) => {
+    console.log('State:', event.state) // "idle" | "listening" | "processing"
   })
+  unlisten = await onResult(async (result) => {
+    console.log('Recognized:', result.transcript, result.isFinal)
+    if (result.isFinal) {
+    // 忽略ts检查
+    // @ts-expect-error 忽略ts检查
+      await speak({ text: result.transcript, language: 'zh-CN' })
+      // 检查关键词
+      const keywords = ['打开', '关闭', '启动', '关闭']
+      const isKeyword = checkoutKeywords(keywords, result.transcript)
+      if (isKeyword) {
+        console.log('包含关键词')
+      }
+      else {
+        console.log('不包含关键词')
+      }
+    }
+  })
+}
+// 关键词
+function checkoutKeywords(textArr: string[], text: string) {
+  if (textArr.includes(text)) {
+    return true
+  }
+  return false
+}
+async function stopListeningAll() {
+  await stopListening()
+  clearAllListeners()
+}
+function clearAllListeners() {
+  const win = window as unknown as Window
+  console.log(win.sttListeners)
+  if (win.sttListeners && Array.isArray(win.sttListeners)) {
+    console.log(`正在清理 ${win.sttListeners.length} 个残留监听器...`)
+    win.sttListeners.forEach((listener) => {
+      if (typeof listener === 'function') {
+        listener()
+      }
+      else if (listener && typeof listener.unregister === 'function') {
+        listener.unregister()
+      }
+    })
+    // 清空数组
+    win.sttListeners = []
+  }
 }
 onMounted(async () => {
   startVoice()
+})
+onBeforeUnmount(async () => {
+  await stopListeningAll()
+
+  unlisten && unlisten()
 })
 </script>
 
@@ -33,6 +93,9 @@ onMounted(async () => {
       <p class="text-lg mx-auto tips">
         通过语音指令快速打开您的软件，提高工作效率。简单说出"打开XX软件"即可启动相应程序。
       </p>
+      <button @click="stopListeningAll">
+        开始录音
+      </button>
     </header>
   </div>
 </template>
